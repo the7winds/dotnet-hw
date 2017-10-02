1ea3621c2c8696102b179af6ab9609fb6741fdba
Unknown <the7winds@yandex.ru>
2017-10-02 17:34:23 +0300

    Implement Option<T> class

--- Option/Option.cs
+++ Option/Option.cs
@@ -6,7 +6,54 @@ using System.Threading.Tasks;
 
 namespace Option
 {
-    public class Option
+    public class Option<T>
     {
+        readonly static Option<T> NONE_OBJECT = new Option<T>();
+        readonly T value;
+
+        private Option() { }
+
+        private Option(T value)
+        {
+            this.value = value;
+        }
+
+        public static Option<T> Some(T value) => new Option<T>(value);
+
+        public static Option<T> None() => NONE_OBJECT;
+
+        public bool IsSome() => this != NONE_OBJECT;
+
+        public bool IsNone() => this == NONE_OBJECT;
+
+        public T Value()
+        {
+            if (IsNone())
+            {
+                throw new Exception("There is no value in None");
+            }
+
+            return value;
+        }
+
+        public Option<U> Map<U>(Func<T, U> f)
+        {
+            if (IsNone())
+            {
+                return Option<U>.None();
+            }
+
+            return Option<U>.Some(f(value));
+        }
+
+        public static Option<T> Flatten(Option<Option<T>> option)
+        {
+            if (option.IsNone())
+            {
+                return Option<T>.None();
+            }
+
+            return option.value;
+        }
     }
 }
